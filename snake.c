8cbe75cd1bc8afca2fd357eb7aabc704a95c8fc0
Jonathan Alves <alves.b.jon@gmail.com>
2025-07-16 17:49:07 -0400

    clean up

--- snake.c
+++ snake.c
@@ -8,10 +8,11 @@
 #define WIN_WIDTH 30
 #define SNAKE_MAX_LENGTH 100
 #define NO_KEY_PRESS -1
-/*
- * TODO
- * - when food updates, cant update on the snake
-*/
+#define GAME_TICK_MS 100
+#define SNAKE_CHAR 'O'
+#define FOOD_CHAR '@'
+#define EMPTY_CHAR ' '
+
 struct Pos {
     int y_axis;
     int x_axis;
@@ -38,14 +39,14 @@ void update_food() {
         food.x_axis = (rand() % (WIN_WIDTH - 2)) + 1;
         valid_position = true;
 
-        for (int i = 0; i <= snake_len - 1; i++) {
+        for (int i = 0; i < snake_len; i++) {
             if (food.y_axis == segments[i].y_axis && food.x_axis == segments[i].x_axis) {
                 valid_position = false;
                 break;
             }
         }
     }
-    mvwaddch(win, food.y_axis, food.x_axis, '@');
+    mvwaddch(win, food.y_axis, food.x_axis, FOOD_CHAR);
 }
 
 void update_snake() {
@@ -55,13 +56,13 @@ void update_snake() {
     for (int i = snake_len - 1; i > 0; i--) {
         segments[i] = segments[i-1];
     }
-    
+
     segments[0].y_axis += dir.y_axis;
     segments[0].x_axis += dir.x_axis;
     head = segments[0];
 
-    mvwaddch(win, prev_tail.y_axis, prev_tail.x_axis, ' ');
-    mvwaddch(win, head.y_axis, head.x_axis, 'O');
+    mvwaddch(win, prev_tail.y_axis, prev_tail.x_axis, EMPTY_CHAR);
+    mvwaddch(win, head.y_axis, head.x_axis, SNAKE_CHAR);
 }
 
 void init() {
@@ -73,7 +74,7 @@ void init() {
     win = newwin(WIN_HEIGHT, WIN_WIDTH, start_y, start_x);
 
     box(win, ACS_VLINE, ACS_HLINE);
-    
+
     keypad(win, TRUE);
     nodelay(win, TRUE);
     curs_set(0);
@@ -83,7 +84,7 @@ void init() {
     update_food();
 }
 
-int quit_game() {
+void quit_game() {
     delwin(win);
     endwin();
     exit(0);
@@ -97,7 +98,7 @@ void process_input() {
     while ((input = wgetch(win)) != ERR) {
         key = input;
     }
-    
+
     switch (key) {
         case KEY_UP:
             if (dir.y_axis != 1) {
@@ -109,10 +110,9 @@ void process_input() {
         case KEY_DOWN:
             if (dir.y_axis != -1) {
                 dir.y_axis = 1;
-                dir.x_axis = -0;
+                dir.x_axis = 0;
             }
             break;
-
 
         case KEY_LEFT:
             if (dir.x_axis != 1) {
@@ -130,7 +130,7 @@ void process_input() {
 
         case 'q':
             quit_game();
-    } 
+    }
 }
 
 void check_collision() {
@@ -140,15 +140,15 @@ void check_collision() {
         snake_len += 1;
         segments[snake_len - 1] = prev_tail;
     }
-    
+
     // snake collision with border
     if (head.y_axis >= WIN_HEIGHT - 1 || head.y_axis <= 0 || head.x_axis >= WIN_WIDTH - 1 || head.x_axis <= 0) {
         quit_game();
     }
 
     //snake collision with itself
-    //we start at i = 1 becuase segments[0] is the head
-    for (int i = 1; i <= snake_len - 1; i++) {
+    //we start at i = 1 because segments[0] is the head
+    for (int i = 1; i < snake_len; i++) {
         if (head.y_axis == segments[i].y_axis && head.x_axis == segments[i].x_axis) {
             quit_game();
         }
@@ -164,7 +164,7 @@ int main(void) {
         check_collision();
 
         wrefresh(win);
-        napms(100);
+        napms(GAME_TICK_MS);
     }
 
     quit_game();
